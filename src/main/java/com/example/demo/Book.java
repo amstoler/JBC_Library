87ee7ecb6bf7411ad78653a47d4fedd734b860cb
amstoler <amstoler@gmail.com>
2018-02-08 10:30:38 -0500

    Initial commit- Created Class called book with defined variables, BookRepository, home controller. application displays sample text from home.html

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+    private String author;
+    private int yop; /*year of publication*/
+    private String isbn; /*stored as string since user may inter dashes*/
+    private String image; /*Will contain a URL*/
+
+    public Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYop() {
+        return yop;
+    }
+
+    public void setYop(int yop) {
+        this.yop = yop;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
