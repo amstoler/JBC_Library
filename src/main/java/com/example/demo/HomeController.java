ea6e61e1dad2c4cb4bb24ccfad4f59d1111dcd1e
Ariel Stoler <amstoler@gmail.com>
2018-02-09 07:18:06 -0500

    Selected ALL Files for push. User can update multiple titles and they will be displayed on bookList

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,7 +2,14 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -11,8 +18,31 @@ public class HomeController {
     BookRepository bookRepository;
 
     @RequestMapping("/")
-    public String sampletext() {
+    public String homepage() {
 
         return "home";
     }
+
+    @RequestMapping("/bookList")
+    public String listBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "bookList";
+    }
+
+    @GetMapping("/add")
+    public String bookForm(Model model){
+        model.addAttribute("book", new Book());
+        return "bookForm";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute("book") Book book, BindingResult result)
+    {
+        if(result.hasErrors()){
+            return "bookForm";
+        }
+        bookRepository.save(book);
+        return "redirect:/add";
+    }
+
 }
